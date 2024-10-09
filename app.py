272f52d25fbdcdda9c83b9cd932ceb6bb50f724a
mbm1337 <mikkelsen96@live.dk>
2024-10-09 14:23:07 +0200

    Streamlit deploy

--- app.py
+++ app.py
@@ -29,23 +29,22 @@ import streamlit as st
 banner = """
     <body style="background-color:yellow;">
             <div style="background-color:#385c7f ;padding:10px">
-                <h2 style="color:white;text-align:center;">Streamlit BI Demo App</h2>
+                <h2 style="color:white;text-align:center;">Assessing Job Automation Risk in the AI Era</h2>
             </div>
     </body>
     """
 
-st.markdown(banner, unsafe_allow_html=True)
+st.markdown(banner, unsafe_allow_html=True
+            )
 
 
 st.markdown(
     """
     ###
         
-    👈 :green[Select a demo case from the sidebar to experience some of what Streamlit can do for BI!]
+    :green[Mustafa & Morten]
     
-    ### To learn more
-    - Check out [Streamlit Documentation](https://docs.streamlit.io)
-    - Contact me by [email](mailto://tdi@cphbusiness.dk)
+    ### 
 """
 )
 
